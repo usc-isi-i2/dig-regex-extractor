71b1cd7a3b2451b549bb260bbb45cee145f95b45
jasonslepicka <jasonslepicka@gmail.com>
2016-10-05 14:41:12 -0700

    Update to dig extractor 0.2.0

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ config = {
     'url': 'https://github.com/usc-isi-i2/dig-regex-extractor',
     'download_url': 'https://github.com/usc-isi-i2/dig-regex-extractor',
     'author_email': 'jasonslepicka@gmail.com',
-    'version': '0.1.3',
+    'version': '0.2.0',
     # these are the subdirs of the current directory that we care about
     'packages': ['digRegexExtractor'],
     'scripts': [],
-    'install_requires':['digExtractor>=0.1.5']
+    'install_requires':['digExtractor>=0.2.0']
 }
 
 setup(**config)
